c27aa7dea88c01cb7be2172946189b3aa493f5f1
boykaaa <tlxc1224@163.com>
2025-10-22 11:32:09 +0800

    登录验证

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
@@ -31,7 +31,11 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
         }
+        console.log('Invalid credentials');
+        return null;
       }
     })
   ]
